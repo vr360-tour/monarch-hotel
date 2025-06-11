e6b1e82e07210115cbe8bc58986c0cbd4c499f44
u9624034 <u9624034@gmail.com>
2025-06-11 16:07:10 +0800

    更新360專案檔案

---
+++ data.js
@@ -0,0 +1,362 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "床正面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1461660618381977,
+          "pitch": 0.45540548591170094,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.7755966148756137,
+          "pitch": 0.5013609723691665,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.635998036934323,
+          "pitch": 0.4812821951624091,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "房間角落",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7605590064413654,
+          "pitch": 0.3913910428249565,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.5759136447399484,
+          "pitch": 0.3141939665349547,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "床右邊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.8580694340987556,
+        "pitch": 0.09406507916141571,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5915583368362327,
+          "pitch": 0.37802863760559013,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 3.069423587951258,
+          "pitch": 0.3609029204388303,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.0443402007226306,
+          "pitch": 0.31311975549746407,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "廁門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9035336634289113,
+          "pitch": 0.5423262109904563,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.9013695543234768,
+          "pitch": 0.4171196633374983,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -1.7550847702853751,
+          "pitch": 0.21687889592978493,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.15176303771743704,
+          "pitch": 0.6626324410169815,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.8080460387211517,
+          "pitch": 0.45976051487380687,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.47721098977439,
+          "pitch": 0.36632108244351613,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.182556163386126,
+          "pitch": -0.08122018649954832,
+          "title": "尊爵大飯店-豪華客房",
+          "text": "<h2 style=\"margin-top: 0.5em; margin-bottom: 0.5em; margin-left: 0.1em; list-style: none; font-weight: 300; font-size: 1.5em; color: rgb(0, 0, 0); text-transform: uppercase; vertical-align: middle; line-height: 1.5; font-family: 微軟正黑體, &quot;Source Sans Pro&quot;, arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; background-color: rgb(255, 255, 255);\">● 14坪(46㎡) | 定價：NT$15,800+10%<br style=\"margin: 0px; padding: 0px; list-style: none;\">房型:客廳+一大床(房間數:2)<br style=\"margin: 0px; padding: 0px; list-style: none;\">每房最多住宿人數為2位<br style=\"margin: 0px; padding: 0px; list-style: none;\">● 16坪(52㎡) | 定價：NT$17,000+10%<br style=\"margin: 0px; padding: 0px; list-style: none;\">房型:客廳+兩小床(房間數:2)<br style=\"margin: 0px; padding: 0px; list-style: none;\">每房最多住宿人數為2位</h2>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "浴室內",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5532911081021616,
+          "pitch": 0.3985998575419085,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.196312613526283,
+          "pitch": 0.6990296833996936,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "淋浴間",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.894448799072274,
+          "pitch": 0.4074979462926329,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.8617383673640333,
+          "pitch": 0.46834811311833313,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "玄關",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.22365768895268978,
+        "pitch": 0.17628110986493084,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5124595629906725,
+          "pitch": 0.5669357748856498,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.28324126923087256,
+          "pitch": 0.46982568712823536,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "尊爵大飯店",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
